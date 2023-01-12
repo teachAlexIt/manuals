8c21491cb412731a5bf9a28cb6084ed320971df5
teachAlexIt <alekseiteacherit@gmail.com>
2023-01-12 22:45:20 +0300

    index and frontend structur

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,3 +1,14 @@
-import a from './test.js';
-let b = a;
-console.log(b)
+const manualsList = document.querySelector('.manuals__list');
+const manualsItems = manualsList.querySelectorAll('.manuals__item');
+
+for (let i = 0; i < manualsItems.length; i++) {
+  manualsItems[i].querySelector('button').onclick = function() {
+    let h = manualsItems[i].scrollHeight;
+    manualsItems[i].classList.toggle('_active');
+    if (manualsItems[i].classList.contains('_active')) {
+      manualsItems[i].style.height = h + "px";
+    }else{
+      manualsItems[i].style.height = 48 + "px";
+    }
+  }
+}
